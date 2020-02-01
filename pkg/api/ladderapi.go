438fdfd23e538d3555d90ab723fa539ea292d842
Pikipoo <anis.bennabi@epitech.eu>
2020-02-01 01:17:40 +0100

    HTML parsing working using tokens

--- pkg/api/ladderapi.go
+++ pkg/api/ladderapi.go
@@ -1,21 +1,37 @@
 package api
 
 import (
+	"container/list"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"golang.org/x/net/html"
 )
 
 func parseLadderHTML(res *http.Response) {
 	doc := html.NewTokenizer(res.Body)
-	for tokenType := doc.Next(); tokenType != html.ErrorToken; {
+	for tokenType := doc.Next(); tokenType != html.ErrorToken; tokenType = doc.Next() {
 		token := doc.Token()
-		if tokenType == html.TextToken && len(token.Data) > 1 {
-			fmt.Println(token.Data)
+		if tokenType == html.StartTagToken && token.Data == "tr" {
+			data := list.New()
+			for tokenType := doc.Next(); tokenType != html.ErrorToken; tokenType = doc.Next() {
+				token := doc.Token()
+				if tokenType == html.EndTagToken && token.Data == "tr" {
+					break
+				}
+				token.Data = strings.Trim(token.Data, "\n ")
+				if tokenType == html.TextToken && token.Data != "" {
+					data.PushBack(token.Data)
+					// fmt.Println(token.Data)
+				}
+			}
+			for e := data.Front(); e != nil; e = e.Next() {
+				fmt.Print(e.Value, " ")
+			}
+			fmt.Println()
 		}
-		tokenType = doc.Next()
 	}
 
 	defer res.Body.Close()
